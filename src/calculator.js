7eefb7a3b86e01e8f64c91b51d1464bc319f8c7b
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-03-04 21:28:19 -0500

    fix linters

--- src/calculator.js
+++ src/calculator.js
@@ -23,22 +23,22 @@ function Calculator() {
         <button type="button" className="button" onClick={() => handleClick('AC')}>AC</button>
         <button type="button" className="button" onClick={() => handleClick('+/-')}>+/-</button>
         <button type="button" className="button" onClick={() => handleClick('%')}>%</button>
-        <button type="button"  className="button operation yellow" onClick={() => handleClick('÷')}>/</button>
-        <button  type="button" className="button" onClick={() => handleClick('7')}>7</button>
-        <button type="button"  className="button" onClick={() => handleClick('8')}>8</button>
-        <button type="button"  className="button" onClick={() => handleClick('9')}>9</button>
-        <button type="button"  className="button operation yellow" onClick={() => handleClick('x')}>x</button>
-        <button  type="button" className="button" onClick={() => handleClick('4')}>4</button>
-        <button type="button"  className="button" onClick={() => handleClick('5')}>5</button>
-        <button  type="button" className="button" onClick={() => handleClick('6')}>6</button>
-        <button type="button"  className="button operation yellow" onClick={() => handleClick('-')}>-</button>
-        <button type="button"  className="button" onClick={() => handleClick('1')}>1</button>
-        <button type="button"  className="button" onClick={() => handleClick('2')}>2</button>
-        <button  type="button" className="button" onClick={() => handleClick('3')}>3</button>
-        <button  type="button" className="button operation yellow" onClick={() => handleClick('+')}>+</button>
-        <button type="button"  className="button zero two-column" onClick={() => handleClick('0')}>0</button>
-        <button type="button"  className="button" onClick={() => handleClick('.')}>.</button>
-        <button type="button"  className="button operation yellow" onClick={() => handleClick('=')}>=</button>
+        <button type="button" className="button operation yellow" onClick={() => handleClick('÷')}>/</button>
+        <button type="button" className="button" onClick={() => handleClick('7')}>7</button>
+        <button type="button" className="button" onClick={() => handleClick('8')}>8</button>
+        <button type="button" className="button" onClick={() => handleClick('9')}>9</button>
+        <button type="button" className="button operation yellow" onClick={() => handleClick('x')}>x</button>
+        <button type="button" className="button" onClick={() => handleClick('4')}>4</button>
+        <button type="button" className="button" onClick={() => handleClick('5')}>5</button>
+        <button type="button" className="button" onClick={() => handleClick('6')}>6</button>
+        <button type="button" className="button operation yellow" onClick={() => handleClick('-')}>-</button>
+        <button type="button" className="button" onClick={() => handleClick('1')}>1</button>
+        <button type="button" className="button" onClick={() => handleClick('2')}>2</button>
+        <button type="button" className="button" onClick={() => handleClick('3')}>3</button>
+        <button type="button" className="button operation yellow" onClick={() => handleClick('+')}>+</button>
+        <button type="button" className="button zero two-column" onClick={() => handleClick('0')}>0</button>
+        <button type="button" className="button" onClick={() => handleClick('.')}>.</button>
+        <button type="button" className="button operation yellow" onClick={() => handleClick('=')}>=</button>
       </div>
     </div>
   );
