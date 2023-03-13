4c614abc2f0b339ba1fa5d043f59bbbf53b15bf2
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-03-12 23:02:40 -0400

    use createRoot

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'),
 );
+
