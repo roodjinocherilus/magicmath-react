af22e4c27fb00d220ff9233acdea8a4e446fab0e
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-03-18 22:00:39 -0400

    wrap the Calculator component in a Router component, and add a Switch component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Calculator from './calculator';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Quotes from './Quotes';
+
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/calculator" component={Calculator} />
+        <Route path="/quotes" component={Quotes} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
+  document.getElementById('root')
 );
