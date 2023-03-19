b56ee3075c807908da033a2a4829b914243d525d
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-03-18 22:13:05 -0400

    create quotes and home

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Calculator App!</h1>
+      <p>This app allows you to perform basic calculations.</p>
+    </div>
+  );
+}
+
+export default Home;
