df5d406daeef68569c8e25562d0fa0c3f1074e99
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-03-18 21:54:31 -0400

    create navbar

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calculator">Calculator</Link></li>
+        <li><Link to="/quotes">Quotes</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
